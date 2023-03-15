4d5d06317adc85359f58abddc8e9e5cd927eccd2
Galia Cantir <galiacantir@gmail.com>
2023-03-15 21:45:50 +0100

    limits condition

---
+++ dino.game/main.js
@@ -0,0 +1,13 @@
+const DINO = document.getElementById("dino");
+const CACTUS = document.getElementById("cactus");
+
+document.addEventListener("keydown", function (event){
+    jump();
+})
+
+function jump(){
+    if(DINO.classList != "jump"){
+        
+    }
+}
+
